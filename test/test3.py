1100915f51cd63c2e5fa74d3f1bd73bc77fc8cf5
yaotony <tony.exmail@gmail.com>
2021-02-11 12:12:57 +0800

    Update test3.py

--- test/test3.py
+++ test/test3.py
@@ -1,9 +1,6 @@
+import time # 引入time
 
-
-
-import matplotlib.font_manager
- 
-a = sorted([f.name for f in matplotlib.font_manager.fontManager.ttflist])
- 
-for i in a:
-    print(i)
+nowTime = int(time.time()) # 取得現在時間
+struct_time = time.localtime(nowTime) # 轉換成時間元組
+timeString = time.strftime("%Y%m%d%I%M%S%P", struct_time) # 將時間元組轉換成想要的字串
+print(timeString)
