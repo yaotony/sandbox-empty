57b5065ae110948e82352d26d54ab6d58c294206
yaotony <tony.exmail@gmail.com>
2021-03-25 15:53:50 +0800

    Revert "LINE"
    This reverts commit 4b4f0067c2d78bcd933c6f4b0800b545f88edc85.

--- test/LineMSG.py
+++ test/LineMSG.py
@@ -1,22 +1,8 @@
 import lineTool
-import requests
 
-token='BGMktHvxXppQctxavvBkKl3aNPjKgG027mWIF4z9a46'
+toke='BGMktHvxXppQctxavvBkKl3aNPjKgG027mWIF4z9a46'
 
- 
-  
-URL = 'https://notify-api.line.me/api/notify'
-  
-  
-def send_message( msg, img=None):
-    """Send a LINE Notify message (with or without an image)."""
-    headers = {'Authorization': 'Bearer ' + token}
-    payload = {'message': msg}
-    files = {'imageFile': open(img, 'rb')} if img else None
-    r = requests.post(URL, headers=headers, params=payload, files=files)
-    if files:
-        files['imageFile'].close()
-    return r.status_code
+def linePush(msg):
+    lineTool.lineNotify(toke,msg)
 
-send_message('tst','c:\\temp\\051.jpg')
-send_message('AAGC')
+
--- test/TT/Order3.py
+++ test/TT/Order3.py
@@ -3,10 +3,8 @@ import time ,datetime
 #定義進場函數，呼號範例為(r,b) = inp(df,r,b,i)
 def inp(time,r,b,note,re):
 
-    note =note + " 下單 ： "+str(r) + ' : '+ str(b) + " Time:"+time.strftime('%Y-%m-%d %H:%M')
-    print(note)
+    note = " 下單 ： "+str(r) + ' : '+ str(b) 
     re.append([time,r,r,r,0,b,0,0,note])
-    note =''
     return (r,b,note,re)
 
 
@@ -16,13 +14,12 @@ def outp(time,Price,r,b,note,re):
     #r是資金存量，b=多空方設定 多方=1 空方=-1
     #price=1代表開盤價，price=4代表收盤價
     rr = (Price - r) * b
-    note = note + ' 出場： b=' + str(b) +' ： 下單：' + str(r) +' , 出場：'+ str(Price) +' ,結算 ： '+str(rr)+ " Time:"+time.strftime('%Y-%m-%d %H:%M')
+    note =' 出場： b=' + str(b) +' ： 下單：' + str(r) +' , 出場：'+ str(Price) +' ,結算 ： '+str(rr)
     re.append([time,r,r,r,0,b,rr,0,note])
-    print(note)
+    #print(note)
     #sendMSG(b,df['Time'].iloc[i],"Exit")
     r=0#歸零
     b=0#多空方歸零
-    note =''
     #linePush( df['note1'].iloc[i])
     return (r,b,rr,note,re)
 
@@ -61,20 +58,18 @@ def stopByS(time,Price,wsp,lsp,r,b,sr,sb,topProfit,note,re):
                 r,b,rr,note,re = outp(time,Price,r,b,note,re)
                 note = note +'強制出場+lsp < mp'  
                 #print('強制出場 - ((topProfit - Price) * b )',((topProfit - Price) * b ),' topProfit=',topProfit,'  Price=',Price )
-                if sb!=0 :
-                    note = '保險單- '              
+                if sb!=0 :              
                     sr,sb,srr,note,re = outp(time,Price,sr,sb,note,re)
                     #print('保險出場： sr= ',sr,' sb=',sb,'  srr=',srr,' Time:',time )
  
         else :
  
-                if sb == 0 and   ((topProfit - r ) * b ) > 5 and  (((topProfit - r ) * b ) * wsp) < (topProfit - Price ) * b:
+                if sb == 0 and   ((topProfit - r ) * b ) > 10 and  (((topProfit - r ) * b ) * wsp) < (topProfit - Price ) * b:
                     # print(' (((topProfit - r ) * b ) * wsp=) ', (((topProfit - r ) * b ) * wsp)  )
                     # print('(topProfit - Price ) * b =', (topProfit - Price ) * b  )
                     # print('topProfit=',topProfit)
                     # print('Price=',Price)
                     # print('r=',r)
-                    note = '保險單- '
                     sr,sb,note,re = inp(time,Price,1,note,re)
                     #print('1 保險進場： sr= ',sr,' sb=',sb,' (Price - r)',(Price - r),' Time:',time )
                 
@@ -124,20 +119,18 @@ def stopByB(time,Price,wsp,lsp,r,b,sr,sb,topProfit,note,re):
                 #print('強制出場 - ((topProfit - Price) * b )',((topProfit - Price) * b ),' topProfit=',topProfit,'  Price=',Price )
                 r,b,rr,note,re = outp(time,Price,r,b,note,re)
                 note = note +'強制出場+lsp < mp'
-                if sb!=0 :
-                    note = '保險單- '                
+                if sb!=0 :                
                     sr,sb,srr,note,re = outp(time,Price,sr,sb,note,re)
                     #print('保險出場： sr= ',sr,' sb=',sb,'  srr=',srr,' Time:',time )  
         else  :
           
 
-            if sb == 0 and ((topProfit - r ) * b ) > 5 and  (((topProfit - r ) * b ) * wsp) < (topProfit - Price ) * b:
+            if sb == 0 and ((topProfit - r ) * b ) > 10 and  (((topProfit - r ) * b ) * wsp) < (topProfit - Price ) * b:
                     # print(' (((topProfit - r ) * b ) * wsp=) ', (((topProfit - r ) * b ) * wsp)  )
                     # print('(topProfit - Price ) * b =', (topProfit - Price ) * b  )
                     # print('topProfit=',topProfit)
                     # print('Price=',Price)
                     # print('r=',r)
-                    note = '保險單- '
                     sr,sb,note,re = inp(time,Price,-1,note,re)
            
                     #print('-1 保險進場： sr= ',sr,' sb=',sb,' (Price - r)',(Price - r),' Time:',time )
